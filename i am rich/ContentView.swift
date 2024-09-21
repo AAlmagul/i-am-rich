033c5692d00fb3e49e7ecb303269076b2ae47f88
AAAlmagul <a_abdigali@kbtu.kz>
2024-09-21 10:46:05 +0500

    Initial Commit

---
+++ i am rich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  i am rich
+//
+//  Created by Алмагуль Абдыгали on 21.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ i am rich/i_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  i_am_richApp.swift
+//  i am rich
+//
+//  Created by Алмагуль Абдыгали on 21.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct i_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
